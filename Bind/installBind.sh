84ff16d5b91d66e744652b9f86bab76ee2799e17
Terry Guldimann <terryguld+gitcommit@gmail.com>
2017-06-09 19:58:52 -0400

    removed extranious sudo commands

--- Bind/installBind.sh
+++ Bind/installBind.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # make the needed directories
-sudo chmod 755 /etc/named
-sudo mkdir /etc/named/zones
+chmod 755 /etc/named
+mkdir /etc/named/zones
 
 # move files nessisary locations
 mv /named.conf /etc/named.conf
@@ -11,10 +11,10 @@ mv /db.cs642-shu.com /etc/named/zones/db.cs642-shu.com
 mv /db.173.31 /etc/named/zones/db.173.31
 
 # check services
-sudo named-checkconf
+named-checkconf
 
 # start services
 /etc/init.d/named start
 
 # configure it to start on startup
-sudo chkconfig named on
+chkconfig named on
